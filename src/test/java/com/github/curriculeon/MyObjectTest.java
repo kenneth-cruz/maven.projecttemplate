419be4257c3ee44bd996a75cc8f27c4da4a6441e
git-leon <xleonhunter@gmail.com>
2020-11-16 16:20:55 -0500

    created demo project for generics; included  method

--- src/test/java/com/github/curriculeon/MyObjectTest.java
+++ src/test/java/com/github/curriculeon/MyObjectTest.java
@@ -3,16 +3,34 @@ package com.github.curriculeon;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyObjectTest {
     @Test
-    public void testRun() { // TODO
-        // Given
-        MyObject myObject = new MyObject();
+    public void testArrayOfPerson() {
+        // given
+        int key = 1;
+        Person person1 = new Person("Leon");
+        Person expected = new Person("Hunter");
+        Person[] personArray = new Person[2];
+        personArray[0] = person1;
+        personArray[1] = expected;
 
         // when
-        myObject.run();
+        Person actual = personArray[key];
 
-        //then
-        Assert.assertNotNull(myObject.toString());
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMapIntegerToPerson() { // TODO
+        Map<Integer, Person> map = new HashMap<>();
+
+    }
+
+    @Test
+    public void testMapPhoneNumberToPerson() { // TODO
     }
 }
